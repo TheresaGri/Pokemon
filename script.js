b6e2b88da8727ecc7e6a7e7417777d7c3d330650
Theresa Grimus <T.Grimus@gmx.net>
2023-01-14 14:59:45 +0100

    .

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 const mainElement = document.querySelector(".main");
 
+//"modern" version
 async function loadFirstPokemon() {
-	const url = "https://pokeapi.co/api/v2/pokemon/1";
-	const response = await fetch(url);
-	const firstPokemonData = await response.json();
-	console.log(firstPokemonData);
+  const url = "https://pokeapi.co/api/v2/pokemon/1";
+  const response = await fetch(url);
+  const firstPokemonData = await response.json();
+  console.log(firstPokemonData);
 
   mainElement.innerHTML += `<h2> ${firstPokemonData.name}</h2>`;
   mainElement.innerHTML += `<h2> ${firstPokemonData.height}</h2>`;
   mainElement.innerHTML += `<img src="${firstPokemonData.sprites.front_default}">`;
 }
 
-loadFirstPokemon();
 
 async function loadPikachu() {
   const url = "https://pokeapi.co/api/v2/pokemon/pikachu";
   const response = await fetch(url);
   const pikachuData = await response.json();
   console.log(pikachuData);
-  mainElement.innerHTML = `<h2>${pikachuData.name}</h2>`;
-  mainElement.innerHTML = `<img src = "${pikachuData.sprites.front_female}">`;
+  mainElement.innerHTML += `<h2>${pikachuData.name}</h2>`;
+  mainElement.innerHTML += `<img src = "${pikachuData.sprites.front_female}">`;
 }
 
+function loadFirstPokemonThen() {
+  const url = "https://pokeapi.co/api/v2/pokemon/1";
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      mainElement.innerHTML += `<h2> ${data.name}</h2>`;
+
+    });
+    
+}
+
+loadFirstPokemon();
 loadPikachu();
+loadFirstPokemonThen();
